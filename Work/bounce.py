88bc648fc62c6d6e5b07fd95be2ec59ee8685c83
Sean <sean@howardabode.com>
2023-02-05 16:40:26 -0500

    First day of work

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,9 @@
 # bounce.py
 #
 # Exercise 1.5
+
+ball_height = 100
+
+for drop in range(1,11):
+    ball_height = ball_height * (3/5)
+    print(drop, round(ball_height, 4))
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,32 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+# mortgage.py
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 1
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+
+while principal > 0:
+    if month in range(extra_payment_start_month, extra_payment_end_month + 1):
+        payment = payment + extra_payment
+    principal = principal * (1+rate/12) - payment
+    if principal < 0:
+        print("You overpaid by", abs(principal))
+        principal = 0
+    total_paid = total_paid + payment
+    if month in range(extra_payment_start_month, extra_payment_end_month + 1):
+        payment = payment - extra_payment
+    print(month, total_paid, principal)
+    month = month + 1
+
+
+print('Total paid', total_paid)
+print('Total months', month - 1)
