af77bd70772c12598ba065479e0c8ef70e80ce49
Sean <sean@howardabode.com>
2023-02-06 19:22:29 -0500

    pcost problem

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,16 @@
 # pcost.py
 #
 # Exercise 1.27
+
+lines = []
+stock_sum = 0
+
+with open('Data/portfolio.csv', 'rt') as f:
+    next(f)
+    for line in f:
+        lines.append(line.split(','))
+
+for line in lines:
+    stock_sum += int(line[1]) * float(line[2])
+
+print(stock_sum)
